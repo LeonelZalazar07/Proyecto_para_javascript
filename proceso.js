23837890becde5736f9895ca65ea989daf3067a2
Leonel Zalazar <leonelzalazar07@gmail.com>
2023-05-16 11:26:39 -0300

    funciones nuevas

--- proceso.js
+++ proceso.js
@@ -1,17 +1,17 @@
-// Capturar entrada de cantidad de zapatillas mediante prompt()
+// entrada de cantidad de zapatillas
 const cantidadDeZapatillas = prompt("Ingrese la cantidad de zapatillas a comprar:");
 
-// Declarar variables y objetos necesarios para simular el proceso seleccionado
+// variables y objetos necesarios
 const precios = [5000, 7000, 9000];
 const descuentoMayorista = 0.2;
 
-// Calcular subtotal
+// calcular subtotal
 const calcularSubtotal = (cantidad, precio) => {
   const subtotal = cantidad * precio;
   return subtotal;
 };
 
-// Calcular descuento por compra mayorista
+// calcular descuento por compra mayorista
 const calcularDescuentoMayorista = (subtotal) => {
   let descuento = 0;
   if (subtotal > 10000) {
@@ -20,31 +20,31 @@ const calcularDescuentoMayorista = (subtotal) => {
   return descuento;
 };
 
-// Calcular total a pagar
+// calcular total a pagar
 const calcularTotal = (subtotal, descuento) => {
   const total = subtotal - descuento;
   return total;
 };
 
-// Obtener el modelo seleccionado por el usuario
+// obtener el modelo seleccionado por el usuario
 const obtenerModelo = () => {
   const modelo = document.getElementById("model").value;
   return modelo;
 };
 
-// Obtener la cantidad de zapatillas ingresada por el usuario
+// obtener la cantidad de zapatillas ingresada por el usuario
 const obtenerCantidad = () => {
   const cantidad = parseInt(document.getElementById("quantity").value);
   return cantidad;
 };
 
-// Obtener el precio del modelo seleccionado
+// obtener el precio del modelo seleccionado
 const obtenerPrecio = (modelo) => {
   const precio = precios[modelo];
   return precio;
 };
 
-// Actualizar el resultado en el DOM
+// actualizar el resultado en el DOM
 const actualizarResultado = (subtotal, descuento, total) => {
   const resultado = document.getElementById("result");
   resultado.innerHTML = `
@@ -54,7 +54,7 @@ const actualizarResultado = (subtotal, descuento, total) => {
   `;
 };
 
-// Función principal que realiza todo el proceso
+// función principal que realiza todo el proceso
 const realizarProceso = () => {
   const modelo = obtenerModelo();
   const cantidad = obtenerCantidad();
@@ -65,5 +65,5 @@ const realizarProceso = () => {
   actualizarResultado(subtotal, descuento, total);
 };
 
-// Asociar la función realizarProceso al evento de clic del botón "Calcular"
+// asociar la función realizarProceso al evento de clic del botón "Calcular"
 document.getElementById("calculate").addEventListener("click", realizarProceso);
